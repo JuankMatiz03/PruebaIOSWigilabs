8550c64ee79dc30c7be0370b30c84fbb0cac3e7d
Leonardo matiz <devleonardom@gmail.com>
2020-05-02 18:12:40 -0500

    header listo

--- pruebaIOS/pruebaIOS/AppDelegate.swift
+++ pruebaIOS/pruebaIOS/AppDelegate.swift
@@ -15,9 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UINavigationBar.appearance().tintColor = UIColor.white
+        UINavigationBar.appearance().backgroundColor = UIColor.black
+        
         return true
     }
 
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
