fff7540bfd32b4753172cbaa33e4453c9ddc4a24
Leonardo matiz <devleonardom@gmail.com>
2020-05-03 23:27:49 -0500

    PruebaIOS Lista.

--- pruebaIOS/pruebaIOS/ViewController.swift
+++ pruebaIOS/pruebaIOS/ViewController.swift
@@ -11,13 +11,38 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var titleAnimation: UILabel!
-        
+    func Alert (Message: String){
+
+        let alert = UIAlertController(title: "¡Alerta!", message: Message, preferredStyle: .alert)
+
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+
+        self.present(alert, animated: true, completion: nil)
+
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        if CheckInternet.Connection(){
+            self.Alert(Message: "Conectado a Internet")
+        }
+        else{
+            self.Alert(Message: "Verifica tu conección a Internet")
+        }
+    }
+    
+    @IBAction func Inicio_valid() {
+        if CheckInternet.Connection() {
+            performSegue(withIdentifier: "Init_app", sender: nil)
+        } else {
+            
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
     }
+
     
 }
 
